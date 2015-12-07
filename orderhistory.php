cb5edff7828171eb8435c8aaa378ada972a86752
edwin tsang <tsangedwin@yahoo.com>
2015-12-06 19:53:01 -0800

    finished orderhistory queries

--- orderhistory.php
+++ orderhistory.php
@@ -5,19 +5,17 @@ $database = "sample";
 $user = "";
 $pass = "";
 $conn = db2_connect($database, $user, $pass);
-echo $_SESSION['CustomerID'];
-//$customerID = $_GET['CurrentUser'];
+
+$customerID = $_SESSION['CustomerID'];
 //$_SESSION['CurrentUser'];
 $id = array();
-$name = array();
-$price = array();
-$description = array();
-$category = array();
-$tagSpecific = array();
+$orderID = array();
+$orderTime = array();
+$itemCost = array();
+$firstName = array();
+$city = array();
 
-
-$sql = "SELECT name, price, description, category, tagSpecific FROM product";
-
+	$sql = "select orderInfo.orderID, orderInfo.orderTime, orderInfo.itemCost, customer.firstName, customer.city from customer join orderInfo on orderInfo.customerID = customer.id where orderInfo.customerID = '" . $customerID ."'";
 	$stmt = db2_prepare($conn, $sql);	
 
 	if ($stmt) {
@@ -28,11 +26,11 @@ $sql = "SELECT name, price, description, category, tagSpecific FROM product";
 			echo "error";
 		}
 		while ($row = db2_fetch_array($stmt)) {
-   			array_push($name, $row[0]);
-   			array_push($price, $row[1]);
-   			array_push($description, $row[2]);
-   			array_push($category, $row[3]);
-   			array_push($tagSpecific, $row[4]);
+   			array_push($orderID, $row[0]);
+   			array_push($orderTime, $row[1]);
+   			array_push($itemCost, $row[2]);
+   			array_push($firstName, $row[3]);
+   			array_push($city, $row[4]);
 		}	
 		/*
 		print_r($name);
@@ -173,33 +171,54 @@ $sql = "SELECT name, price, description, category, tagSpecific FROM product";
     var oldShippingCost = 0
     var tax = 0;
     $(document).ready(function() {
+	/*
+	$orderID = array();
+$orderTime = array();
+$itemCost = array();
+$firstName = array();
+$city = array();
+<?php echo json_encode($productID); ?>
+*/
         // do a query to retrieve all the productIDs, etc from this user's cart and store them into arrays
         // Example of a cart with stuff in it after doing a query. Something like
         // "select cart.productID, product.name, product.price, product.description, product.category, product.tag specific from cart join product on product.productID=cart.productID where cart.ID = [whatever the logged in customer's ID is];"
-        var productIDs = [1000000001, 1000000002, 1000000009, 1000000010];
+      /*
+		var productIDs = [1000000001, 1000000002, 1000000009, 1000000010];
         var prices = [19.99, 249.99, 219.99, 19.99];
         var names = ["Volcom Frickin Chino Pants", "Michael Kors 1224 Suit", "Tommy Hilfiger Black Classic-Fit Tuxedo Suit", "John Ashford Long-Sleeve Herringbone Flannel Shirt"];
         var categories = ["Pants", "Suit", "Suit", "Shirt"];
         var tagSpecifics = ["Men's", "Men's", "Men's", "Men's"];
         var description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur";
-        createProductTable(productIDs, prices, names, categories, tagSpecifics); // pass the array(s) containing all the productIDs from the cart
+        */
+		
+		var orderIDs = <?php echo json_encode($orderID); ?>;
+		var orderTimes = <?php echo json_encode($orderTime); ?>;
+		var totals = <?php echo json_encode($itemCost); ?>; 
+		var firstNames = <?php echo json_encode($firstName); ?>;
+		var cities = <?php echo json_encode($city); ?>;
+		
+		createProductTable(orderIDs, orderTimes, totals, firstNames, cities); // pass the array(s) containing all the productIDs from the cart
     });
 
-    function createProductTable(productIDs, prices, names, categories, tagSpecifics) {
+    function createProductTable(orderIDs, orderTimes, totals, firstNames, cities) {
 	//  
+		
         var html = ""; // we append all our html stuff into here
         cost = 0; // creates a productTable for every item in the cart
-        for (i = 0; i < productIDs.length; i++) {
+        for (i = 0; i < orderIDs.length; i++) {		
+		var totalTemp = parseFloat(totals[i]);
+		totalTemp = Math.round(totalTemp * 100) / 100;
+
         html += '<div class = "' + 
-		productIDs[i] + '"><table class="table"><thead><tr><th colspan="2" style="width:30%;">Order #'
-		+ productIDs[i]
+		orderIDs[i] + '"><table class="table"><thead><tr><th colspan="2" style="width:30%;">Order #'
+		+ orderIDs[i]
 		+ '</th><th style="40%;">Info</th><th>Total</th></tr></thead><tbody><tr><td></div></td><td><br><br><button class="btn btn-success" type="button" onclick="redirect('
-		+ productIDs[i] + ');">Order Details</button>  </td><td><table class="table"><tr><td>Date Ordered:</td><td>'
-		+ '0985175' + '</td></tr><tr><td>Ship To:</td><td>' 
-		+ categories[i] + '</td></tr><td>City:</td><td>'
-		+ tagSpecifics[i] + '</table></td><td>'
-		+ '$' + prices[i] + '<br><div class="caption"><p></div></td></tr></tbody></table><br></div>';
-            cost = cost + prices[i];
+		+ orderIDs[i] + ');">Order Details</button>  </td><td><table class="table"><tr><td>Date Ordered:</td><td>'
+		+ orderTimes[i] + '</td></tr><tr><td>Ship To:</td><td>' 
+		+ firstNames[i] + '</td></tr><td>City:</td><td>'
+		+ cities[i] + '</table></td><td>'
+		+ '$' + totalTemp.toFixed(2) + '<br><div class="caption"><p></div></td></tr></tbody></table><br></div>';
+        
         }
 		//<a href="#/" onclick="removeCartEntry(' 
 		//+ productIDs[i] +',' + prices[i] + ');">Remove from cart</a>
