758c42cb415a464db2f93c549340dddfde34e02f
jardinargent <stellarebullience@gmail.com>
2015-12-06 17:34:27 -0800

    item.php
    fixed

--- item.php
+++ item.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 $database = "sample";
 $user = "";
 $pass = "";
@@ -99,15 +100,22 @@ $sql = "SELECT name, price, description, category, tagSpecific FROM product WHER
                         <a href="contact.html">Contact</a>
                     </li>
                 </ul>
-                <ul class="nav navbar-nav pull-right">
+                 <ul class="nav navbar-nav pull-right">
+		
+		<?php if(!isset($_SESSION['CurrentUser'])){ ?>	
                     <li>
-                        <a href="login.html">Login/SignUp</a>
+                        <a href="login.php">Login/SignUp</a>
+                    </li> <?php } ?>
+		<?php if(isset($_SESSION['CurrentUser'])){?>
+		    <li>
+                        <a href="php/logout.php"><?php echo $_SESSION['CurrentUser']." (logout)"?></a>
                     </li>
-                    <li>
-                        <a href="cart.html">
+                    <li> 
+                        <a href="cart.php">
                             <img src="http://findicons.com/files/icons/1700/2d/512/cart.png" alt="cartImage" style="width:20px; height=20px;">
-                        </a>
-                    </li>
+                        </a> 
+                    </li> <?php } ?>
+
                 </ul>
             </div>
             <!-- /.navbar-collapse -->
