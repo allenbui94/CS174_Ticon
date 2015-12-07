c78adefe7aa8f86f46d74182d164aacbe5a65245
edwin tsang <tsangedwin@yahoo.com>
2015-12-07 05:28:50 -0800

    more queries

--- php/paymentValidate.php
+++ php/paymentValidate.php
@@ -17,17 +17,18 @@ $expiration = $month .  "/" . $year;
 $productID = array();
 $productPrice = array();
 
+$cProductID = "";
 //echo $shipping;
 
 $sql = "insert into payment values('$customerID','$cardNumber','$cvc', '$expiration','$cardName','')";
 $sql2 ="select cart.productID, product.price from cart join product on product.productID = cart.productID where customerID ='$customerID'";
 $sql3 = "select count(orderID) from orderInfo";	
-
+//need a for loop to store all orderedItems 
+$sql4 = "insert into orderedItems values('$orderID','$cProductID'";
 $stmt = db2_prepare($conn, $sql);	
 
 if ($stmt) {
 	$result = db2_execute($stmt);
-	
 	$stmt = db2_prepare($conn, $sql2);	
 	if ($stmt) {
 		$result = db2_execute($stmt);
@@ -43,9 +44,13 @@ if ($stmt) {
 		while($row = db2_fetch_array($stmt)){
 			$maxNumber= $row[0];
 		} 
-	}
-	
+	}	
 	$orderID = strval(1000+$maxNumber+1);
+	
+	$stmt = db2_prepare($conn, $sql4);
+	//for all productIDs, do this
+	$result = db2_execute($stmt);
+	
 	echo $orderID;
 	//print_r($productID);
 	
